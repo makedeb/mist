07b6ba74d3c0988544946c5868e9ea02875cb52a
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-06-06 05:03:30 -0500

    Fix location of built binary in PKGBUILD

--- .drone/scripts/create-release.sh
+++ .drone/scripts/create-release.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
+if echo "${DRONE_COMMIT_MESSAGE}" | grep -q 'GH SKIP'; then
+    echo "Skipping GitHub release creation!"
+    exit 0
+fi
+
 .drone/scripts/setup-pbmpr.sh
 sudo apt-get install gh parse-changelog -y
 
@@ -9,3 +14,5 @@ source makedeb/PKGBUILD
 release_notes="$(parse-changelog CHANGELOG.md "${pkgver}")"
 echo "${github_api_key}" | gh auth login --with-token
 gh release create --target "${DRONE_COMMIT_SHA}" -n "${release_notes}" "v${pkgver}"
+
+# vim: set sw=4 expandtab:
