133cd28475d9a7e67cab32bfa5ab4fdb1187a432
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-09-25 03:13:58 -0500

    Remove testing files from repo
    Leftover from running local tests

--- test.rs
+++
@@ -1,6 +0,0 @@
-use std::os::unix::fs::PermissionsExt;
-
-fn main() {
-    let file = std::fs::File::open("target/debug/mist").unwrap();
-    println!("{:o}", file.metadata().unwrap().permissions().mode());
-}
